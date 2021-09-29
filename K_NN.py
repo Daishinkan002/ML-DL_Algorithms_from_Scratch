499fc666d9fa318d595d42c12d1f1ab06191813e
daishinkan002 <shantanutripathi002@gmail.com>
2021-09-29 18:52:47 +0530

    Structured KNN

--- K_NN.py
+++ K_NN.py
@@ -3,88 +3,92 @@ import numpy as np
 import matplotlib as plt
 import random
 import math
+import sys
+from sklearn.model_selection import train_test_split
 
 
 
 
-def load_dataset(dataset,split,training_set,testing_set):
-    for x in range(len(dataset)-1):
-        for y in range(len(dataset[0])-1):
-            dataset[x][y] = float(dataset[x][y])
-        if random.random() < split:
-            training_set.append(dataset[x])
-        else:
-            testing_set.append(dataset[x])
+class KNN(object):
+
+    def __init__(self, k=3, algorithm='auto'):
+        self.k = k
+        self.X_train = []
+        self.y_train = []
+        self.algorithm=algorithm
+    
+    def euclidean_distance(self,a,b):
+        dist = 0
+        length = len(b)
+        for i in range(length):
+            dist += (a[i]-b[i])**2
+        res = math.sqrt(dist)
+        return res
+    
+    def fit(self, X, y):
+        self.X_train = X_train.to_numpy()
+        self.y_train = y_train.to_numpy()
+    
+    def predict(self, X_test):
+        X_test = X_test.to_numpy()
+        length = len(X_test)
+        y_pred = []
+        for i in range(length):
+           y_pred.append(self.predict_single(X_test[i]))
+        return y_pred
+
+    def predict_single(self, x):
+        if(self.algorithm=='auto'):
+            distance_list = []
+            for i in range(len(self.X_train)):
+                dist = self.euclidean_distance(x, self.X_train[i])
+                distance_list.append([self.y_train[i],dist])
+            distance_list.sort(key = lambda x:x[1])
+            neighbours = distance_list[0:self.k]
+            output = self.majority_vote(neighbours)
+            return output
+
+
+    def majority_vote(self, neighbours):
+        a = []
+        dictionary = {}
+        for i in range(self.k):
+            label = neighbours[i][0]
+            try:
+                dictionary[label] += 1
+            except:
+                dictionary[label] = 1
+        
+        max_value = 0
+        max_occuring_label = []
+        output = ''
+        for key, value in dictionary.items():
+            if(value > max_value):
+                output = key
+                max_value = value
+        return output
+
+    
 
 
 
-def euclidean_distance(a,b,length):
-    dist = 0
-    for i in range(length):
-        dist += (a[i]-b[i])**2
-    res = math.sqrt(dist)
-    #print(res)
-    return res
-
-def predict_neighbour(training_set,prediction_data, k):
-    distance_list = []
-    length = len(prediction_data)-1
-    for i in range(len(training_set)):
-        dist = euclidean_distance(prediction_data,training_set[i],length)
-        distance_list.append([training_set[i],dist])
-    distance_list.sort(key = lambda x:x[1])
-    #print(distance_list,"\n\n")
-    neighbours = distance_list[0:k]
-    a = []
-    for i in range(k):
-        a.append(distance_list[i][0])
-    #print(a)
-    return a
-
-def vote(point_list):
-    vote_list = {}
-    for i in range(len(point_list)):
-        vote = point_list[i][-1]
-        if vote in vote_list:
-            vote_list[vote]+=1
-        else:
-            vote_list[vote]=1
-
-           
-    sorted_vote = sorted(vote_list.items(),key=lambda x:x[1], reverse=True)
-    #print(sorted_vote)
-    #print(vote_list,sorted_vote)
-    #print("Sorted_Vote = ",sorted_vote[0])
-    return(sorted_vote[0])
-
-
-
-def find_acc(testing_set,inferences):
-    correct = 0
-    length = len(testing_set)
-    for i in range(length):
-        print(testing_set[i][-1],inferences[i])
-        if testing_set[i][-1] == inferences[i]:
-            correct +=1
-    return 100*correct/float(length)
+    def score(self, X,y):
+        correct = 0
+        length = len(X)
+        y = y.to_numpy()
+        y_pred = self.predict(X)
+        for i in range(length):
+            if(y_pred[i] == y[i]):
+                correct +=1
+        return 100*correct/float(length)
 
 
 if __name__ == "__main__":
-    dataset = pd.read_csv("Iris.csv")
-    #features = dataset[['RI','Na','Mg','Al','Si','Type']]
-    data=dataset.values.tolist()
-    training_set = []
-    testing_set = []
-    validate = []
-    load_dataset(data,0.67,training_set,testing_set)
-    k = 6   
-    #print(testing_set)
-    for i in range(len(testing_set)):
-        closest = predict_neighbour(training_set,testing_set[i],k)
-        print("Closest = ",closest,"\ntraining set [i] = ",testing_set[i],"\n\n\n")
-        res  = vote(closest)
-        #print("RES = ",res,"\n\n\n")
-        validate.append(res[0])
-        #print(res[1],' ====== ',testing_set[i][-1] )
-    accuracy = find_acc(testing_set,validate)
-    print("Accuracy = ",accuracy)
+    df = pd.read_csv('Datasets/Iris.csv')
+    X = df.drop(['Id', 'Species'], axis=1)
+    y = df['Species']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    k = 5
+    model = KNN(k)
+    model.fit(X_train, y_train)
+    print(f"Model Accuracy (when k = {k}) is {model.score(X_test, y_test)}")
